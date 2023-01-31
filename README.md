414c286b8974c0624576f017651904ec0b3e1a93
moassamo <moassermo@gmail.com>
2023-01-31 09:54:22 -0600

    seven wonder project complete

---
+++ README.md
@@ -0,0 +1,45 @@
+# seven-wonders
+
+## Description
+Aerial view of each wonder using the Google Maps API and a list of location details including hotels, restaurants and attractions near it using the TripAdvisor API. 
+Our motivation for this app is to get people interested in learning about the new 7 Wonders for 2023 and inspire them to go visit these beautiful locations. 
+As an uninspired traveler, I want to be able to research and view new places of interest for upcoming travel. 
+
+## Installation
+
+https://github.com/coreybutler/nvm-windows/releases/download/1.1.10/nvm-setup.exe - Download node - nvm install 16 - nvm use 16.19.0 - node --version - npm install cors-anywhere - node corshelper.js
+
+## Usage
+![Home Page](/Assets/images/Screenshot-readMe.png)
+![Wonder 1](/Assets/images/Screenshot1stWonder.png)
+
+## Credits
+
+https://github.com/ItsMARPON  -  https://github.com/kduesler  -  https://github.com/mmohamed12452
+
+
+Article
+https://www.cntraveler.com/gallery/new-seven-wonders-of-the-world-2023
+
+Perito Moreno Glacier, Argentina
+https://en.wikipedia.org/wiki/Perito_Moreno_Glacier
+
+AlUla, Saudi Arabia
+https://www.arabnews.com/node/1970951
+http://127.0.0.1:5501/wonder3/wonder3.html
+
+Tiger's Nest Monastery, Bhutan
+https://www.culturaobscura.com/climbing-tigers-nest/
+
+Cappadocia, Turkey
+https://en.wikipedia.org/wiki/Cappadocia
+https://www.google.com/search?q=Cappadocia,+Turkey&rlz=1C1ONGR_enUS1031US1031&sxsrf=AJOqlzW9zVBHbpLaGteRsCIaPVXXGrSlww:1674857946154&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiZ67eH5Oj8AhW2EkQIHbb1CbYQ_AUoAXoECAEQAw&biw=1920&bih=929&dpr=1#imgrc=0coNViioIFbVYM
+
+The Lake District, Great Britain
+https://www.lonelyplanet.com/articles/guide-to-the-lake-district
+
+The Sardine Run, South Africa
+https://diveintolife.blog/sardine-run-south-africa/
+
+## License
+MIT License
---
+++ wonder3/assets/js/TripAdvisor3JS.js
@@ -0,0 +1,50 @@
+let TripApiKey = "29CB0A3EE8A74953A71A7964758809DA";
+const contentContainer = document.getElementById("content");
+
+const options = {
+  method: "GET",
+  headers: { accept: "application/json" },
+  cors: "no-cors",
+};
+
+// TripAdvisor API - Location Search API must first obtain unique Location ID, fetch function to obtain Location ID.
+
+fetch(
+  "http://localhost:8080/https://api.content.tripadvisor.com/api/v1/location/search?key=29CB0A3EE8A74953A71A7964758809DA&searchQuery=AlUla%2C%20Saudi%20Arabia&category=Hotels&language=en",
+  options
+)
+  .then((response) => response.json())
+  .then(function (data) {
+    console.log(data);
+
+    tripAdvisorDetails(data.data);
+  });
+
+// TripAdvisor API - Location Details API requires Location ID to return comprehensive information about a location, fetch function to obtain details.
+
+function tripAdvisorDetails(data) {
+  for (var i = 0; i < 4; i++) {
+    fetch(
+      "http://localhost:8080/https://api.content.tripadvisor.com/api/v1/location/" +
+        data[i].location_id +
+        "/details?key=29CB0A3EE8A74953A71A7964758809DA&language=en&currency=USD"
+    )
+      .then((response) => response.json())
+      .then(function (data) {
+        if (data) {
+          console.log("SUCCESS", data);
+          const card = document.createElement("div");
+          card.classList.add("box");
+          card.innerHTML = `             
+          <div id="ta-name"><p>Name: ${data.name}</p></div>
+          <div id="ta-address"><p>Address: ${data.address_obj.address_string}</p></div>
+          <div id="ta-description"><p>Category: ${data.category.name}</p></div>
+          <div id="ta-rating"><p>Rating: ${data.rating}</p></div>
+          <button id="ta-button" class="button">
+          <a href="${data.web_url}">See More</a>
+          </button>`;
+          contentContainer.appendChild(card);
+        }
+      });
+  }
+}
